a6b6c2c4aa9890856ecbdc38f14766d687ecc55f
Rikki Valverde <rikkivalverde@gmail.com>
2019-01-30 22:01:58 -0800

    didn't commit some files

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,19 +1,78 @@
-from styx_msgs.msg import TrafficLight
+import tensorflow as tf
+import numpy as np
+import matplotlib
 
-class TLClassifier(object):
+class TrafficLightClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        pass
+        PATH_TO_MODEL = 'frozen_inference_graph.pb'
+        self.detection_graph = tf.Graph()
+        with self.detection_graph.as_default():
+            od_graph_def = tf.GraphDef()
+            # Works up to here.
+            with tf.gfile.GFile(PATH_TO_MODEL, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+            self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+            self.d_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+            self.d_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+            self.d_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+            self.num_d = self.detection_graph.get_tensor_by_name('num_detections:0')
+        self.sess = tf.Session(graph=self.detection_graph)
 
-    def get_classification(self, image):
-        """Determines the color of the traffic light in the image
 
-        Args:
-            image (cv::Mat): image containing the traffic light
+    def get_classification(self, img):
+        # Bounding Box Detection.
+        with self.detection_graph.as_default():
+            # Expand dimension since the model expects image to have shape [1, None, None, 3].
+            img_expanded = np.expand_dims(img, axis=0)  
+            (boxes, scores, classes, num) = self.sess.run(
+                [self.d_boxes, self.d_scores, self.d_classes, self.num_d],
+                feed_dict={self.image_tensor: img_expanded})
+#         return boxes, scores, classes, num
 
-        Returns:
-            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
+        list_ind = np.where(classes[0]==10)
+        if len(list_ind[0]) > 0:
+            ind = list_ind[0][0]
+        else:
+            return 100
 
-        """
-        #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        ymin = boxes[0][ind][0] 
+        xmin = boxes[0][ind][1]
+        ymax = boxes[0][ind][2]
+        xmax = boxes[0][ind][3]
+
+        im_height = 600
+        im_width = 800
+        (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
+                                      ymin * im_height, ymax * im_height)
+
+        imageh = matplotlib.colors.rgb_to_hsv(img)
+
+        tl = imageh[int(top):int(bottom),int(left):int(right),2]
+
+        on = np.where(tl<200)
+        tl[on] = 0
+        (ymean, xmean) = np.median(np.transpose(np.nonzero(tl)),0)
+        height = bottom - top
+
+        if (ymean < height/3):
+            tcolor = 0
+        elif (ymean > 2.0/3*height):
+            tcolor = 2
+        else:
+            tcolor = 1
+
+        return tcolor
+
+
+
+
+
+# from PIL import Image
+#from tl_classifier import TrafficLightClassifier
+# tl = TrafficLightClassifier()
+# image = Image.open('0003.jpg')
+# tl.get_classification(image)
+
+
